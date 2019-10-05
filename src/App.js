0741c01f849e776af8bd78afc93131e04427a628
Rodney-Race <rodneyrace@gmail.com>
2019-10-04 21:07:44 -0400

    update

--- src/App.js
+++ src/App.js
@@ -1,10 +1,16 @@
 import React from 'react';
 import './App.css';
 import { Home, About, Contact, Navigation } from './components';
+import { Route } from 'react-router-dom';
 
 const App = () => (
   <div>
     <Navigation />
+      <Route exact path="/" component={Home}/>
+      <Route exact path="/about"components={About}/>
+      <Route path ="/contact"  components={Contact}/>  
+
+
   </div>
 );
 
